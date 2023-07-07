4e75b2a6a0b8625397a5a9f2ccb0e25ceba554ff
yuki-okamoto-13 <okamoto.yuki@eversense.co.jp>
2023-07-08 08:44:18 +0900

    add project

---
+++ jems-guild/jems-guild/jems_guildApp.swift
@@ -0,0 +1,17 @@
+//
+//  jems_guildApp.swift
+//  jems-guild
+//
+//  Created by okamoto yuki on 2023/07/08.
+//
+
+import SwiftUI
+
+@main
+struct jems_guildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ jems-guild/jems-guildUITests/jems_guildUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  jems_guildUITestsLaunchTests.swift
+//  jems-guildUITests
+//
+//  Created by okamoto yuki on 2023/07/08.
+//
+
+import XCTest
+
+final class jems_guildUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
